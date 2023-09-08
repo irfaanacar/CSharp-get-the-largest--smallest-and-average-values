1afc3e639ed5979271ddf12f02b7294c83d18d33
Irfan <irfaanacar@gmail.com>
2023-09-08 23:44:03 +0300

    CSharp Windows Form Finding the largest, smallest and average values
    In this application, I coded a program that shows the largest, smallest, average of the entered values and which of the entered values are even or odd. By the way, sorry for my English.

---
+++ Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace enbykenkckortalamadegerbulma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblortalama = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 44);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(209, 88);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(285, 276);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 56);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orchid;
+            this.button2.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(12, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 56);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "EN BÜYÜK";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button3.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(12, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 56);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "EN KÜÇÜK";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Crimson;
+            this.button4.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(12, 252);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 56);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "TOPLAM";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.YellowGreen;
+            this.button5.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(12, 308);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(169, 56);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "ORTALAMA";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LightSalmon;
+            this.button6.Font = new System.Drawing.Font("Oswald", 10F, System.Drawing.FontStyle.Bold);
+            this.button6.Location = new System.Drawing.Point(268, 582);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(169, 56);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "ÇİFTLER";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Gray;
+            this.button7.Font = new System.Drawing.Font("Oswald", 10F, System.Drawing.FontStyle.Bold);
+            this.button7.Location = new System.Drawing.Point(607, 582);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(169, 56);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "TEKLER";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(209, 380);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(285, 196);
+            this.listBox2.TabIndex = 9;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(539, 380);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(285, 196);
+            this.listBox3.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(517, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 29);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "EN BÜYÜK:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(517, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "EN KÜÇÜK:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(517, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 29);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "ORTALAMA:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(531, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 29);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "TOPLAM:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(611, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(22, 29);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(613, 156);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(22, 29);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(613, 211);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 29);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "0";
+            // 
+            // lblortalama
+            // 
+            this.lblortalama.AutoSize = true;
+            this.lblortalama.Font = new System.Drawing.Font("Oswald", 10.2F, System.Drawing.FontStyle.Bold);
+            this.lblortalama.Location = new System.Drawing.Point(613, 266);
+            this.lblortalama.Name = "lblortalama";
+            this.lblortalama.Size = new System.Drawing.Size(22, 29);
+            this.lblortalama.TabIndex = 18;
+            this.lblortalama.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(849, 647);
+            this.Controls.Add(this.lblortalama);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblortalama;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace enbykenkckortalamadegerbulma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);
+            textBox1.Clear();
+            textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = new int[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                sayilar[i] = int.Parse(listBox1.Items[i].ToString());
+            }
+            Array.Sort(sayilar);//burada sayılar küçükten büyüğe sıralandı
+            label5.Text = sayilar[sayilar.Length - 1].ToString();//ve burada da dizideki toplam eleman uzunluğundan 1 çıkartıldı. 1 çıkartıldığında sonuncu elemanı sonuncu indeksi verir.
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = new int[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                sayilar[i] = int.Parse(listBox1.Items[i].ToString());
+            }
+            Array.Sort(sayilar);
+            label6.Text = sayilar[0].ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = new int[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                sayilar[i] = int.Parse(listBox1.Items[i].ToString());
+            }
+            int toplam = 0;
+            foreach (int sayi in sayilar)
+            {
+                toplam = toplam + sayi;
+                label7.Text = toplam.ToString();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = new int[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                sayilar[i] = int.Parse(listBox1.Items[i].ToString());
+            }
+            int toplam = 0;
+            foreach (int sayi in sayilar)
+            {
+                toplam = toplam + sayi;
+                lblortalama.Text = (toplam / sayilar.Length).ToString();
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = new int[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                sayilar[i] = int.Parse(listBox1.Items[i].ToString());
+            }
+            foreach (int i in sayilar)
+            {
+                if (i % 2 == 0)
+                {
+                    listBox2.Items.Add(i);
+                }
+            }
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int[] sayilar = new int[listBox1.Items.Count];
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                sayilar[i] = int.Parse(listBox1.Items[i].ToString());
+            }
+            foreach (int i in sayilar)
+            {
+                if (i % 2 == 1)
+                {
+                    listBox3.Items.Add(i);
+                }
+            }
+        }
+    }
+}
